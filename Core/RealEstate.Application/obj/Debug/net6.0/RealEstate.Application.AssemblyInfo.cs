c7e14cfc633b4295d0fae9c055011af43420a420
Thanaraj Vinayagamoorthy <thanarajv@opusxenta.com>
2024-10-22 15:46:40 +0530

    Assembly

--- Core/RealEstate.Application/obj/Debug/net6.0/RealEstate.Application.AssemblyInfo.cs
+++ Core/RealEstate.Application/obj/Debug/net6.0/RealEstate.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstate.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c268a1e8a73fb38c41ce422ddd14b8673d7b6a02")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstate.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstate.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
